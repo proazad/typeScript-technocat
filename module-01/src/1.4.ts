a920a1cbea089be1246d0e772169a4cf3dbd7767
proazad <azadhossain7595.com>
2024-05-01 01:20:32 +0300

    I learn TypeScript Data Types String, Number, undefined, Null, Array and Tuple

---
+++ module-01/src/1.4.ts
@@ -0,0 +1,23 @@
+// String type data declare
+const firstName: String = "Azad";
+
+// Number type data declare
+const age: Number = 28;
+
+//Boolean Data type declare
+const isAdmin: Boolean = true;
+
+//Undifiend Data Type Declare
+const x: undefined = undefined;
+
+//Null Type Date Declare
+const y: null = null;
+
+//Array
+// Declare String Type Array
+const friends: string[] = ["Kawsar", "Javed"];
+// Declare Number Type Array
+const eligibleRollList: Number[] = [1, 3, 4, 5, 6];
+
+//Declare Number & String Type Array
+const student: [string, number, boolean] = ["Azad Hossain", 7, true];
